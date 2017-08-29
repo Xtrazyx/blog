46b785c22cc5aa66ed99150c75c75f50dd3eefa7
Xtrazyx <xtrazyx@gmail.com>
2017-08-29 17:13:40 +0200

    Controller class, Twig Trait, Doctrine Trait

---
+++ src/JHD/Framework/Controller.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Xtrazyx
+ * Date: 16/08/2017
+ * Time: 15:07
+ */
+
+namespace JHD\Framework;
+
+use Symfony\Component\HttpFoundation\Request;
+
+abstract class Controller
+{
+    use Twig, DoctrineEntityManager;
+
+    /**
+     * @param Request $request
+     */
+    abstract function action(Request $request);
+}
